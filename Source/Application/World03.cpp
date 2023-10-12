3cd274ae2e1a900dd480239f310255abb45e8f6b
BadAtRainbow6 <118947416+BadAtRainbow6@users.noreply.github.com>
2023-10-12 10:43:10 -0600

    heheheha

--- Source/Application/World03.cpp
+++ Source/Application/World03.cpp
@@ -1,7 +1,7 @@
 #include "World03.h"
 #include "Framework/Framework.h"
-#include "Renderer/Renderer.h"
 #include "Input/InputSystem.h"
+#include <glm/glm/gtc/type_ptr.hpp>
 
 #define INTERLEAVE
 
@@ -9,17 +9,19 @@ namespace nc
 {
     bool World03::Initialize()
     {
-        m_program = GET_RESOURCE(Program, "shaders/unlit_color.prog");
+        m_program = GET_RESOURCE(Program, "shaders/unlit_texture.prog");
         m_program->Use();
 
-#ifdef INTERLEAVE
+        m_texture = GET_RESOURCE(Texture, "textures/llama.jpg");
+        m_texture->Bind();
+        m_texture->SetActive(GL_TEXTURE0);
 
         //vertex data
         float vertexData[] = {
-            -0.8f, -0.8f, 0.0f, 1.0f, 0.0f, 0.0f,
-            -0.8f, 0.8f, 0.0f, 0.0f, 1.0f, 0.0f,
-            0.8f, -0.8f, 0.0f, 0.0f, 0.0f, 1.0f,
-            0.8f, 0.8f, 0.0f, 1.0f, 1.0f, 1.0f,
+            -0.8f, -0.8f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+            -0.8f,  0.8f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+             0.8f, -0.8f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+             0.8f,  0.8f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
         };
 
         GLuint vbo;
@@ -30,7 +32,7 @@ namespace nc
         glGenVertexArrays(1, &m_vao);
         glBindVertexArray(m_vao);
 
-        glBindVertexBuffer(0, vbo, 0, 6 * sizeof(GLfloat));
+        glBindVertexBuffer(0, vbo, 0, 8 * sizeof(GLfloat));
 
         //position
         glEnableVertexAttribArray(0);
@@ -42,45 +44,10 @@ namespace nc
         glVertexAttribFormat(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat));
         glVertexAttribBinding(1, 0);
 
-#else
-
-        //vertex data
-        float positionData[] = {
-            -0.8f, -0.8f, 0.0f,
-            -0.8f, 0.8f, 0.0f,
-            0.8f, -0.8f, 0.0f,
-            0.8f, 0.8f, 0.0f,
-        };
-
-        float colorData[] = {
-            1.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-        };
-
-        GLuint vbo[2];
-        glGenBuffers(1, vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(positionData), positionData, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colorData), colorData, GL_STATIC_DRAW);
-
-        glGenVertexArrays(1, &m_vao);
-        glBindVertexArray(m_vao);
-
-        //position
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-        glBindVertexBuffer(0, vbo[0], 0, 3 * sizeof(GLfloat));
-
-        //color
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-        glBindVertexBuffer(1, vbo[1], 0, 3 * sizeof(GLfloat));
-
-#endif
+        //texcoord
+        glEnableVertexAttribArray(2);
+        glVertexAttribFormat(2, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat));
+        glVertexAttribBinding(2, 0);
 
         return true;
     }
@@ -91,26 +58,40 @@ namespace nc
 
     void World03::Update(float dt)
     {
-        m_angle += 90 * dt;
-        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? m_speed * +dt : 0;
-        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? m_speed * -dt : 0;
-        m_position.z += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? m_speed * -dt : 0;
-        m_position.z += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? m_speed * +dt : 0;
+        ENGINE.GetSystem<Gui>()->BeginFrame();
+
+        ImGui::Begin("Transform");
+        ImGui::DragFloat3("Position", &m_transform.position[0]);
+        ImGui::DragFloat3("Rotation", &m_transform.rotation[0]);
+        ImGui::DragFloat3("Scale", &m_transform.scale[0]);
+        ImGui::End();
+
+        //m_transform.rotation.z += 180 * dt;
+
+        m_transform.position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? m_speed * +dt : 0;
+        m_transform.position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? m_speed * -dt : 0;
+        m_transform.position.z += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? m_speed * -dt : 0;
+        m_transform.position.z += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? m_speed * +dt : 0;
         m_time += dt;
 
+        m_program->SetUniform("offset", glm::vec2{ m_time, 0 });
+        m_program->SetUniform("tiling", glm::vec2{ 2, 2 });
+
         // model matrix
-        glm::mat4 position = glm::translate(glm::mat4{ 1 }, m_position);
+        /*glm::mat4 position = glm::translate(glm::mat4{ 1 }, m_position);
         glm::mat4 rotation = glm::rotate(glm::mat4{ 1 }, glm::radians(m_angle), glm::vec3{0, 0, 1});
-        glm::mat4 model = position * rotation;
-        m_program->SetUniform("model", model);
+        glm::mat4 model = position * rotation;*/
+        m_program->SetUniform("model", m_transform.GetMatrix());
 
         //view matrix
-        glm::mat4 view = glm::lookAt(glm::vec3{ 0, 4, -5 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 });
+        glm::mat4 view = glm::lookAt(glm::vec3{ 0, 0, 3 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 });
         m_program->SetUniform("view", view);
 
         // projection
         glm::mat4 projection = glm::perspective(glm::radians(70.0f), 800.0f / 600.0f, 0.01f, 100.0f);
         m_program->SetUniform("projection", projection);
+
+        ENGINE.GetSystem<Gui>()->EndFrame();
     }
 
     void World03::Draw(Renderer& renderer)
@@ -121,6 +102,8 @@ namespace nc
         // render
         glBindVertexArray(m_vao);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+        ENGINE.GetSystem<Gui>()->Draw();
 
         // post-render
         renderer.EndFrame();
